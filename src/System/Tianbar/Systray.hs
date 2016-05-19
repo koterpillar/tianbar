0a7f377cedf15a22ef99f99ef03f009bdefaa02e
Alexey Kotlyarov <a@koterpillar.com>
2016-05-19 18:15:37 +1000

    Add systray

--- src/System/Tianbar/Systray.hs
+++ src/System/Tianbar/Systray.hs
@@ -3,10 +3,10 @@
 module System.Tianbar.Systray ( systrayNew ) where
 
 import GI.Gdk.Objects.Display
-import GI.Gdk.Objects.Screen
 
 import GI.Gtk
-import GI.Gtk.Enums
+
+import Graphics.UI.Gtk.Misc.TrayManager
 
 import System.Tianbar.Configuration
 
@@ -14,14 +14,14 @@ systrayNew :: IO Widget
 systrayNew = do
     box <- boxNew OrientationHorizontal 5
 
-    -- trayManager <- trayManagerNew
-    -- disp <- displayGetDefault
-    -- screen <- displayGetScreen disp (fromIntegral myScreen)
-    -- _ <- undefined trayManagerManageScreen trayManager screen
+    trayManager <- trayManagerNew
+    Just disp <- displayGetDefault
+    screen <- displayGetScreen disp (fromIntegral myScreen)
+    _ <- trayManagerManageScreen trayManager screen
 
-    -- _ <- on trayManager undefined trayIconAdded $ \w -> do
-    --     widgetShowAll w
-    --     boxPackStart box w False False 0
+    _ <- onTrayIconAdded trayManager $ \w -> do
+        widgetShowAll w
+        boxPackStart box w False False 0
 
     widgetSetSizeRequest box (-1) (fromIntegral barHeight)
 
