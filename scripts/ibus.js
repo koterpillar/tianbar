bb5aa13d15955d9c231458dd2daad874654dc0aa
Alexey Kotlyarov <a@koterpillar.com>
2016-05-27 21:58:25 +1000

    Display an icon for IBus

--- scripts/ibus.js
+++ scripts/ibus.js
@@ -14,12 +14,54 @@ define(['jquery', './dbus'], function ($, dbus) {
 
   self.updated = $.Callbacks();
 
+  // IBus DBus connection
+  var bus;
+
   // Display the status of the input method
-  function display(engine) {
+  function display() {
     var widget = self.widget();
 
-    widget.text(engine);
-    self.updated.fire(engine);
+    bus.call({
+      'destination': 'org.freedesktop.IBus',
+      'path': '/org/freedesktop/IBus',
+      'iface': 'org.freedesktop.DBus.Properties',
+      'member': 'Get',
+      'body': [
+        'string:org.freedesktop.IBus',
+        'string:GlobalEngine'
+      ]
+    }).then(function (result) {
+      var body = result.body[0];
+      var imeName = body[3];
+      var languageCode = body[5];
+      var imeIcon = body[8];
+      var imeSymbol = body[12];
+
+      widget.empty();
+
+      if (imeSymbol) {
+        widget.text(imeSymbol);
+      } else if (imeIcon) {
+        if (!/\//.test(imeIcon)) {
+          imeIcon = '/usr/share/icons/hicolor/scalable/apps/' + imeIcon + '.svg';
+        }
+        widget.append($('<img/>')
+                      .attr('src', 'tianbar:///root/' + imeIcon)
+                      .css({'height': '20px'}));
+      } else {
+        widget.text(languageCode);
+      }
+
+      widget
+        .attr('title', imeName)
+        .css({
+          'display': 'inline-block',
+          'vertical-align': 'middle',
+          'width': '25px'
+        });
+
+      self.updated.fire();
+    });
   }
 
   $(document).ready(function () {
@@ -29,11 +71,10 @@ define(['jquery', './dbus'], function ($, dbus) {
       }
     }).then(function (address) {
       address = address.trim();
-      dbus.connect(address).then(function (bus) {
-        bus.listen({ member: 'GlobalEngineChanged' }).add(function (result) {
-          var engine = result.body[0];
-          display(engine);
-        });
+      dbus.connect(address).then(function (connectedBus) {
+        bus = connectedBus;
+        display();
+        bus.listen({ member: 'GlobalEngineChanged' }).add(display);
       });
     });
   });
