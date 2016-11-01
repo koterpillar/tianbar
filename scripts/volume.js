3f8e23b28f0a12d826d8e2fd0c5eba3d6e5a4d04
Alexey Kotlyarov <a@koterpillar.com>
2016-11-01 15:26:14 +1100

    Use DBus for querying PulseAudio

--- scripts/volume.js
+++ scripts/volume.js
@@ -4,13 +4,8 @@
  *
  * Requires 'jquery' to be available through RequireJS.
  */
-define(['jquery', './socket'], function ($, socket) {
+define(['jquery', './dbus'], function ($, dbus) {
   "use strict";
-
-  const UID_RE = /Uid:\t(\d+)/;
-
-  const VOLUME_RE = /set-sink-volume.+ ([^ \n]+)/;
-  const MUTE_RE = /set-sink-mute.+ ([^ \n]+)/;
 
   const MAX_VOLUME = 0x10000;
 
@@ -22,13 +17,6 @@ define(['jquery', './socket'], function ($, socket) {
   const self = {};
 
   self.settings_command = 'gnome-control-center sound';
-
-  function uid() {
-    return $.ajax('tianbar:///root/proc/self/status')
-    .then(function (result) {
-      return +UID_RE.exec(result)[1];
-    });
-  }
 
   self.widget = () => $('.widget-volume');
 
@@ -103,31 +91,57 @@ define(['jquery', './socket'], function ($, socket) {
     widget.attr('title', percentage + '%');
   };
 
-  $.when(
-    $.ajax('tianbar:///execute', {
-      data: {
-        command: 'pacmd load-module module-cli-protocol-unix'
+  $.ajax('tianbar:///execute', {
+    data: {
+      command: 'pacmd load-module module-dbus-protocol'
+    }
+  }).then(function () {
+    return dbus.session.getProperty(
+      'org.PulseAudio1',
+      '/org/pulseaudio/server_lookup1',
+      'org.PulseAudio.ServerLookup1',
+      'Address'
+    );
+  }).then(function (bus_address) {
+    return dbus.connect(bus_address);
+  }).then(function (bus) {
+    function refresh() {
+      function get_device_property(sink, name) {
+        return bus.getProperty(
+          'org.PulseAudio.Core1',
+          sink,
+          'org.PulseAudio.Core1.Device',
+          name
+        );
       }
-    }),
-    uid()
-  ).then(function (_, uid) {
-    return socket('/var/run/user/' + uid + '/pulse/cli');
-  }).then(function (pulseSocket) {
-    pulseSocket.recv.add(function (dump) {
-      self.mute = MUTE_RE.exec(dump)[1] === "yes";
-      self.volume = parseInt(VOLUME_RE.exec(dump)[1], 16) / MAX_VOLUME;
 
-      self.display();
+      bus.getProperty(
+        'org.PulseAudio.Core1',
+        '/org/pulseaudio/core1',
+        'org.PulseAudio.Core1',
+        'Sinks'
+      ).then(function (sinks) {
+        if (sinks.length === 0) {
+          // No sound
+          return [[0], true];
+        } else {
+          return $.when(
+            get_device_property(sinks[0], 'Volume'),
+            get_device_property(sinks[0], 'Mute')
+          );
+        }
+      }).then(function (volumes, mute) {
+        // volume is an array of channels' volumes, average it
+        const volume = volumes.reduce((a, b) => a + b, 0) / volumes.length;
+        self.volume = volume / MAX_VOLUME;
+        self.mute = mute;
+        self.display();
 
-      window.setTimeout(requestDump, 1000);
-
-    });
-
-    function requestDump () {
-      pulseSocket.send('dump\n');
+        window.setTimeout(refresh, 1000);
+      });
     }
 
-    requestDump();
+    refresh();
   });
 
   $(document).ready(function () {
